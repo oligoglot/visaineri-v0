a2f7922e6b4743a0bc38812447276b09986efe03
Sundar <sundar@dataweave.com>
2020-10-19 18:56:50 +0530

    adding source directories

---
+++ src/FiniteStateMachine.java
@@ -0,0 +1,190 @@
+head	1.1;
+access;
+symbols;
+locks; strict;
+comment	@# @;
+
+
+1.1
+date	2004.08.17.08.34.45;	author lsundar;	state Exp;
+branches;
+next	;
+
+
+desc
+@@
+
+
+1.1
+log
+@Java Source files mainly generated from JavaCC, JavaCC files to be added later
+@
+text
+@import javax.swing.*;
+import javax.swing.text.StyledDocument;
+import javax.swing.text.BadLocationException;
+import java.io.*;
+
+public class FiniteStateMachine {
+	private final char Thaema = '0';
+	private final char Pulima = '1';
+	private final char Karuvilam = '2';
+	private final char Koovilam = '3';
+	
+	private final char Thaemangai = '4';
+	private final char Pulimangai = '5';
+	private final char Karuvilangai = '6';
+	private final char Koovilangai = '7';
+
+	private final char Naal = 'N';
+	private final char Malar = 'M';
+	private final char Kaasu = 'K';
+	private final char Pirappu = 'P';
+
+	private final char Kutriyalugaram = 'Z';
+
+	private int state = 0;
+
+	public FiniteStateMachine(JTextPane outputArea) throws Exception{
+		int val;
+		int kuralNo=0;
+        boolean error = false;
+        boolean isKutriyalugaram = false;
+			InputStreamReader in = new InputStreamReader(new FileInputStream("output2thalai"));
+
+			while ( (val = in.read()) != -1 ) {
+
+				if(val == Kutriyalugaram)
+				{
+					isKutriyalugaram = true;
+				}
+				else
+				{
+					switch (state) {
+					case 0:
+						switch (val) {
+							case Thaema:
+							case Pulima:
+									state = 1;
+									break;
+
+							case Koovilam:
+							case Karuvilam:
+							case Thaemangai:
+							case Pulimangai:
+							case Karuvilangai:
+							case Koovilangai:
+									state = 2;
+									break;
+							case 10:
+									break;
+							default:
+                                   	StyledDocument doc=(StyledDocument)outputArea.getDocument();
+                                    doc.insertString(doc.getLength(),"Error at kural no::"+kuralNo+"\n",doc.getStyle("Error"));
+                                    doc.insertString(doc.getLength(),"Expecting Thema|Pulima|Koovilam|Karuvilam|Thaemangai|Pulimangai|Karuvilangai|Koovilangai.\n",doc.getStyle("Error"));
+                                    error=true;
+                                    throw new Exception("Expecting Thema|Pulima|Koovilam|Karuvilam|Thaemangai|Pulimangai|Karuvilangai|Koovilangai.");
+
+						}
+						break;
+
+					case 1:
+						switch(val) {
+							case Pulima:
+									state=1;
+									break;
+
+							case Pulimangai:
+							case Karuvilam:
+							case Karuvilangai:
+									state=2;
+									break;
+
+							case Malar:
+							case Pirappu:
+									state=3;
+									break;
+
+							default:
+									if(isKutriyalugaram)
+									{isKutriyalugaram = false;}
+									else
+									{
+										StyledDocument doc=(StyledDocument)outputArea.getDocument();
+                                        doc.insertString(doc.getLength(),"Error at kural no::"+kuralNo+"\n",doc.getStyle("Error"));
+                                        doc.insertString(doc.getLength(),"Expecting MaaMunNirai.Received MaaMunNer\n",doc.getStyle("Error"));
+                                        error=true;
+                                        throw new Exception("Expecting MaaMunNirai.Received MaaMunNer");
+
+									}
+
+
+						 }
+						break;
+
+					case 2:
+						switch(val){
+							case Thaema:
+									state=1;
+									break;
+
+							case Thaemangai:
+							case Koovilam:
+							case Koovilangai:
+									state=2;
+									break;
+
+							case Naal:
+						        case Kaasu:
+									state=3;
+									break;
+							default:
+									if(isKutriyalugaram)
+                                        {isKutriyalugaram = false;}
+									else
+									{
+									    StyledDocument doc=(StyledDocument)outputArea.getDocument();
+                                        doc.insertString(doc.getLength(),"Error at kural no::"+(kuralNo-1)+"\n",doc.getStyle("Error"));
+                                        doc.insertString(doc.getLength(),"Expecting VilaMunNer or KaiMunNer\n",doc.getStyle("Error"));
+                                        error=true;
+                          				throw new Exception("Expecting VilaMunNer or KaiMunNer");
+
+									}
+
+							}
+						break;
+
+
+
+					case 3:
+							if(val == 13)
+							{
+								state = 0;
+								kuralNo++;
+							}
+							else
+							{
+                                StyledDocument doc=(StyledDocument)outputArea.getDocument();
+                                doc.insertString(doc.getLength(),"Error at kural no::"+kuralNo+"\n",doc.getStyle("Error"));
+                                doc.insertString(doc.getLength(),"Invalid state\n",doc.getStyle("Error"));
+                                error=true;
+                           		throw new Exception("Invalid state");
+
+                            }
+							break;
+
+
+				}
+
+				if((char)val == Kutriyalugaram)
+				{
+					isKutriyalugaram = true;
+				}
+
+			}
+		}        
+	}
+}
+
+		
+@
---
+++ src/MainFrame.java
@@ -0,0 +1,328 @@
+head	1.1;
+access;
+symbols;
+locks; strict;
+comment	@# @;
+
+
+1.1
+date	2004.08.17.08.34.45;	author lsundar;	state Exp;
+branches;
+next	;
+
+
+desc
+@@
+
+
+1.1
+log
+@Java Source files mainly generated from JavaCC, JavaCC files to be added later
+@
+text
+@
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+
+
+public class MainFrame extends JFrame{
+    JTextPane inputArea;
+    JTextPane outputArea;
+    JTextArea outputText;
+    JScrollPane inputPanel;
+    JScrollPane outputPanel;
+    StyledDocument inputDoc;
+    StyledDocument outputDoc;
+    JMenuBar optionsMenu;
+    JPanel optionsPanel;
+    JToolBar optionsToolbar;
+    JFileChooser fc;
+    Style fontStyle;
+
+    MainFrame()
+    {
+              super("Visai Neri");
+              fc=new JFileChooser();
+              fc.setCurrentDirectory(new File("./"));
+              addMenu();
+              getContentPane().setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
+              inputDoc=new DefaultStyledDocument();
+              inputArea=new JTextPane(inputDoc);
+              inputArea.setFont(new Font("MylaiTSC",Font.PLAIN,16));
+              inputPanel=new JScrollPane(inputArea);
+              inputPanel.setPreferredSize(new Dimension(250,350));
+              inputPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createTitledBorder("Input Text")));
+              outputDoc=new DefaultStyledDocument();
+              outputArea=new JTextPane(outputDoc);
+              fontStyle=outputArea.addStyle("Error",null);
+              StyleConstants.setForeground(fontStyle,Color.red);
+              StyleConstants.setBold(fontStyle,true);
+              fontStyle=outputArea.addStyle("Success",null);
+              StyleConstants.setForeground(fontStyle,Color.blue);
+              StyleConstants.setBold(fontStyle,true);
+              StyleConstants.setItalic(fontStyle,true);
+              fontStyle=outputArea.addStyle("Status",null);
+              StyleConstants.setForeground(fontStyle,Color.black);
+              //outputText=new JTextArea(10,30);
+              outputPanel=new JScrollPane(outputArea);
+              outputPanel.setPreferredSize(new Dimension(250,30));
+              outputPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createTitledBorder("Output")));
+              addToolbar();
+              getContentPane().add(inputPanel);
+              getContentPane().add(outputPanel);
+              setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+              setSize(500,600);
+              setVisible(true);
+    }
+
+    private void openFile()
+    {
+         int returnVal=fc.showOpenDialog(getContentPane());
+         if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File file = fc.getSelectedFile();
+                if(file.exists()){
+                    inputArea.setText("");
+                    try {
+                        FileReader fReader=new FileReader(file);
+                        BufferedReader bReader=new BufferedReader(fReader);
+                        String text=new String("");
+                        String temp;
+                        temp = bReader.readLine();
+                         while(temp!=null){
+                             text+=temp;
+                             text+="\n";
+                             temp=bReader.readLine();
+                         }
+                        inputArea.setText(text);
+                        bReader.close();
+                    }
+                    catch(FileNotFoundException e)
+                    {
+
+                    }
+                    catch(IOException e)
+                    {
+
+                    }
+
+                }
+
+         }
+    }
+
+
+    private void saveFile()
+    {
+               boolean saved=false;
+               while(!saved){
+               int returnVal = fc.showSaveDialog(getContentPane());
+               if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File file = fc.getSelectedFile();
+                    if(file.exists()){
+                        int n = JOptionPane.showConfirmDialog(
+                        fc,
+                         "Doing this will replace the existing file",
+                        "Ensure",
+                        JOptionPane.YES_NO_OPTION);
+                        if(n==0){
+                            try {
+                                PrintWriter printer=new PrintWriter(new FileWriter(file));
+                                printer.write(inputArea.getText());
+                                printer.close();
+                                saved=true;
+                            } catch (IOException e1) {
+                                 e1.printStackTrace();
+                              }
+                        }
+                    }
+                    else{
+                        try {
+                            PrintWriter printer=new PrintWriter(new FileWriter(file));
+                            printer.write(inputArea.getText());
+                            printer.close();
+                            saved=true;
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                          }
+                    }
+              }
+              else{
+                   saved=true;
+               }
+    }
+    }
+
+    private void addMenu()
+    {
+              optionsMenu=new JMenuBar();
+              JMenu menu;
+              JMenuItem menuItem;
+              menu =new JMenu("File");
+              menuItem=new JMenuItem("Open");
+              menuItem.addActionListener(new ActionListener()
+              {
+                  public void actionPerformed(ActionEvent evt)
+                  {
+                      openFile();
+                  }
+              }
+              );
+              menu.add(menuItem);
+              menuItem=new JMenuItem("Save");
+              menuItem.addActionListener(new ActionListener()
+              {
+                  public void actionPerformed(ActionEvent evt)
+                  {
+                      saveFile();
+                  }
+              }
+              );
+              menu.add(menuItem);
+              menuItem=new JMenuItem("Exit");
+              menuItem.addActionListener(new ActionListener()
+              {
+                  public void actionPerformed(ActionEvent evt)
+                  {
+                      System.exit(0);
+                  }
+              }
+              );
+              menu.add(menuItem);
+              optionsMenu.add(menu);
+              menu=new JMenu("Actions");
+              menuItem=new JMenuItem("Parse");
+              menuItem.addActionListener(new ActionListener()
+              {
+                  public void actionPerformed(ActionEvent evt)
+                  {
+                    parseVenpa();
+                  }
+              });
+              menu.add(menuItem);
+              menuItem=new JMenuItem("Clear");
+              menuItem.addActionListener(new ActionListener()
+              {
+                  public void actionPerformed(ActionEvent evt)
+                  {
+                    clearText();
+                  }
+              });
+              menu.add(menuItem);
+              optionsMenu.add(menu);
+              setJMenuBar(optionsMenu);
+    }
+
+    private void addToolbar()
+    {
+              optionsToolbar=new JToolBar();
+              optionsToolbar.setFloatable(false);
+              optionsToolbar.setLayout(new BoxLayout(optionsToolbar,BoxLayout.X_AXIS));
+              JButton button;
+              button=new JButton("LOAD");
+              button.addActionListener(new ActionListener()
+              {
+                public void actionPerformed(ActionEvent evt)
+                {
+                    openFile();
+                }
+              });
+              optionsToolbar.add(button);
+              button=new JButton("SAVE");
+              button.addActionListener(new ActionListener()
+              {
+                public void actionPerformed(ActionEvent evt)
+                {
+                    saveFile();
+                }
+              });
+              optionsToolbar.add(button);
+              button=new JButton("PARSE");
+              button.addActionListener(new ActionListener()
+              {
+                  public void actionPerformed(ActionEvent evt)
+                  {
+                        parseVenpa();
+                  }
+              });
+              optionsToolbar.add(button);
+              button=new JButton("CLEAR");
+              button.addActionListener(new ActionListener()
+              {
+                  public void actionPerformed(ActionEvent evt)
+                  {
+                       clearText();
+                  }
+              });
+              optionsToolbar.add(button);
+              optionsToolbar.add(Box.createHorizontalGlue());
+              getContentPane().add(optionsToolbar);
+    }
+
+    private void parseVenpa()
+    {
+        PrintWriter printer=null;
+        outputArea.setText("");
+
+
+        try {
+                printer = new PrintWriter(new FileWriter("Venpa.bare"));
+                printer.write(inputArea.getText());
+                printer.close();
+            } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            TsciiParser.entryPoint("Venpa.bare",outputDoc);
+            ParserThalai.entryPoint("output.b4",outputArea);
+            FiniteStateMachine fsm = new FiniteStateMachine(outputArea);
+			try 
+			{
+				StyledDocument outputDoc= (StyledDocument) outputArea.getDocument();
+				outputDoc.insertString(outputDoc.getLength(),"Given verse(s) satisfies the CFG for Venpa. For more Information regarding CFG for Venpa refer:\n Context Free Grammar for Natural Language Constructs - An implementation for Venpa Class of Tamil Poetry(URL:http://www.infitt.org/ti2003/papers/19_raman.pdf)",outputDoc.getStyle("Success"));
+			} 
+			catch (BadLocationException e) 
+			{
+				e.printStackTrace();
+			}
+
+        } catch (Exception pe)
+        {
+			try 
+			{
+				StyledDocument outputDoc= (StyledDocument) outputArea.getDocument();
+				outputDoc.insertString(outputDoc.getLength(),"Given verse(s) do(es) not satisfy the CFG for Venpa.\n",outputDoc.getStyle("Error"));
+				outputDoc.insertString(outputDoc.getLength(),"For more Information regarding CFG for Venpa refer:\n Context Free Grammar for Natural Language Constructs - An implementation for Venpa Class of Tamil Poetry(URL:http://www.infitt.org/ti2003/papers/19_raman.pdf)",outputDoc.getStyle("Success"));
+			} 
+			catch (BadLocationException e) 
+			{
+				e.printStackTrace();
+			}
+        }
+
+
+
+
+    }
+
+    private void clearText()
+    {
+        inputArea.setText("");
+		outputArea.setText("");
+    }
+
+    public static void main(String args[])
+    {
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        setDefaultLookAndFeelDecorated(true);
+        new MainFrame();
+    }
+
+
+}
+@
